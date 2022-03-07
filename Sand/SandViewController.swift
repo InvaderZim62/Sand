1167fe1075a562708107671489ef2312345e34c5
Invader Zim <pwdelete@gmail.com>
2022-03-07 15:30:07 -0800

    Added view, scene, and camera.

--- Sand/SandViewController.swift
+++ Sand/SandViewController.swift
@@ -11,4 +11,39 @@ import SceneKit
 
 class SandViewController: UIViewController {
     
+    private var scnView: SCNView!
+    private var scnScene: SCNScene!
+    private var cameraNode: SCNNode!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupScene()
+        setupCamera()
+    }
+
+    // MARK: - Setup
+    
+    private func setupView() {
+        scnView = self.view as? SCNView
+        scnView.allowsCameraControl = true  // use standard camera controls with swiping
+//        scnView.showsStatistics = false
+        scnView.autoenablesDefaultLighting = true
+        scnView.isPlaying = true  // prevent SceneKit from entering a "paused" state, if there isn't anything to animate
+//        scnView.delegate = self  // needed for renderer, below
+    }
+    
+    private func setupScene() {
+        scnScene = SCNScene()
+        scnScene.background.contents = "Background_Diffuse.png"
+//        scnScene.physicsWorld.contactDelegate = self  // requires SCNPhysicsContactDelegate (extension, below)
+        scnView.scene = scnScene
+    }
+    
+    private func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 30)
+        scnScene.rootNode.addChildNode(cameraNode)
+    }
 }
